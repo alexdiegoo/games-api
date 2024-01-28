2a04b2bdc0ac6ccdadec3e78428a4eeebc6912df
Alex Diego <66486335+alexdiegoo@users.noreply.github.com>
2024-01-28 14:40:56 +0000

    config: config celebrate

--- src/shared/infra/http/routes/register.routes.ts
+++ src/shared/infra/http/routes/register.routes.ts
@@ -1,11 +1,13 @@
+import { celebrate } from 'celebrate';
 import { Router } from 'express';
 
 import { RegisterGameController } from '../../../../modules/games/useCases/registerGame/RegisterGameController';
+import { registerValidator } from '../../../../modules/games/useCases/registerGame/validator';
 
 const registerRoutes = Router();
 
 const registerController = new RegisterGameController();
 
-registerRoutes.post('/', registerController.handle);
+registerRoutes.post('/', celebrate(registerValidator), registerController.handle);
 
 export { registerRoutes };
